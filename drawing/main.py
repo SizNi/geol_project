6d0541b97259bc8a651f48bf210485c81de5115d
SizNi <wwwwwwwww@list.ru>
2023-02-07 02:22:09 +0300

    add scale with scaling depend on well depth

--- drawing/main.py
+++ drawing/main.py
@@ -6,6 +6,7 @@ width, height = 2480, 3508
 # коэффициент пересчета мм -> % от общей ширины/высоты (2480/210 = 11,809 (1 мм в пикселях))
 # 2480/100 = 24,8 пикселя в 1%, соответственно 1 мм = 11,809/24,8 = 0,4761% (по y)
 # 3508/100 = 35,08 пикселя в 1%, соответственно 1 мм = 11,809/35,08 = 0,3366% (по x)
+# общая высота рисунка - 270 мм
 mm_x = 0.004761
 mm_y = 0.003366
 koef = 2480/210
@@ -18,6 +19,14 @@ def main():
     r_0 = draw.Rectangle(0, 0, 210*koef, 297*koef,
                          fill='white', stroke='black')
     d.append(r_0)
+    header(d)
+    scale(d, 48)
+    d.savePng('example.png')
+
+# Создание наименований таблицы
+
+
+def header(d):
     # d x1, y1, x2, y2
     # создание заголовка таблицы (надо немного поправить перенос строк, для таблицы
     # можно сделать его вручную)
@@ -34,9 +43,39 @@ def main():
     rectangle(d, 163, 297-15, 45, -10, 'Конструкция скважины', 'h')
     rectangle(d, 163, 297-25, 22.5, -15, 'Диаметр, мм', 'h')
     rectangle(d, 185.5, 297-25, 22.5, -15, 'Глубина, м', 'h')
-    d.savePng('example.png')
 
 
+# риусет масштабную линейку слева
+def scale(d, well_depth):
+    # выбираем масштаб
+    if well_depth <= 50:
+        section = 5
+    elif well_depth > 50 and well_depth <= 150:
+        section = 10
+    else:
+        section = 20
+    # количество делений шкалы (section/2 для округления в большую сторону)
+    section_numbers = round((well_depth + (section/2))/section)
+    # колчичество мм на секцию (вся шкала растягивается на 250 мм)
+    mm_section = 250 / section_numbers
+    y_start = (297 - 15 - 25 - mm_section)
+    for number in range(section_numbers):
+        # строим прямоугольник
+        # rectangle(d, 10, 297-15, 12, -25, 'Масштаб', 'v')
+        r = draw.Rectangle(10*koef, y_start*koef, 12*koef, mm_section*koef,
+                           fill='white', stroke='black')
+        d.append(r)
+        # проводим линию для текста (совпадает с низом прямоугольника)
+        p = draw.Lines((10)*koef, (y_start + 1)*koef,
+                       (22)*koef, (y_start + 1)*koef,
+                       close=False,
+                       stroke='white')
+        text = str((number+1)*section)
+        d.append(draw.Text([text], 40, path=p, text_anchor='middle'))
+        y_start -= mm_section
+
+
+# создание прямоугольника с текстом
 def rectangle(d, x, y, x1, y1, text, direction):
     r = draw.Rectangle(x*koef, y*koef, x1*koef, y1*koef,
                        fill='white', stroke='black')
@@ -58,21 +97,23 @@ def rectangle(d, x, y, x1, y1, text, direction):
     else:
         number_str = math.ceil(abs((len(text)*2.1)/y1))
         i = 0
-        # шаг с которым будем смещать строки
+        # Первоначальное положение строки
         step = y1 / number_str
         text_start_step = 0
         text_step = round(len(text)/abs(number_str))
         # ввод строк внутрь квадрата
-        # -1 нужен чтоб не было проблем выходом за пределы строки
+        # -1 нужен чтоб не было проблем с выходом за пределы итератора
         # из-за округления
         while i < number_str-1:
             p = draw.Lines((x)*koef, (y+step+1)*koef,
                            (x+x1)*koef, (y+step+1)*koef,
                            close=False,
                            stroke='white')
+            insert = str(text[text_start_step:text_step]) + '-'
             d.append(
-                draw.Text([text[text_start_step:text_step]], 40, path=p, text_anchor='middle'))
-            y += step
+                draw.Text([insert], 40, path=p, text_anchor='middle'))
+            # смещение относительно первоначальной строки
+            y += -5
             i += 1
             text_start_step = text_step
             text_step += text_step
--- drawing/test.py
+++ drawing/test.py
@@ -1,6 +1,3 @@
-text = 'Описание пород1234567891011121314'
-start = 0
-step = len(text)/3
-print(len(text))
-print(round(11.0))
-print(text[10:])
+x = 50
+section_numbers = round((x+2.5)/5)
+print(section_numbers)
