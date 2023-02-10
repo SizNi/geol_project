9b8f9a6b735c1921f0eeeffc1be9123f012bc8f4
SizNi <wwwwwwwww@list.ru>
2023-02-10 18:27:20 +0300

    add filter part in filter_well

--- drawing/main.py
+++ drawing/main.py
@@ -11,13 +11,29 @@ mm_x = 0.004761
 mm_y = 0.003366
 koef = 2480/210
 # шаблон данных для слоев
-data = {'layers': {
+# из бд будет прихдить список
+layers_data = {'layers': {
     1: {'id': 1, 'name': 'Q', 'thick': 45.0, 'sediments': 'Глина серая, желтая'},
     2: {'id': 2, 'name': 'J\u2083', 'thick': 10, 'sediments': 'Песок мелко-среднезернистый'},
     3: {'id': 3, 'name': 'J\u2083ox-c', 'thick': 15, 'sediments': 'Глина'},
     4: {'id': 4, 'name': 'C\u2083g-P\u2081a', 'thick': 25, 'sediments': 'Известняк'},
 }
 }
+well_data = {'well_data': {
+    'columns': {
+        1: {'id': 1, 'D': 377, 'from': 0.0, 'till': 34.0, 'type': 'обсадная'},
+        2: {'id': 2, 'D': 273, 'from': 0.0, 'till': 74.0, 'type': 'обсадная'},
+        3: {'id': 3, 'D': 273, 'from': 59.0, 'till': 95.0, 'type': 'фильтровая', 'filter': {
+            1: {'id': 1, 'from': 75.0, 'till': 79.0},
+            2: {'id': 2, 'from': 85.0, 'till': 90.0},
+        }}
+    },
+    'pump_type': 'ЭЦВ-6-10-110',
+    'pump_depth': 55.0,
+    'static_lvl': 32.0,
+    'dynamic_lvl': 35.0,
+    'well_depth': 95.0
+}}
 
 
 def main():
@@ -32,11 +48,25 @@ def main():
     d.append(r)
     header(d)
     scale(d, 95)
-    layers(d, 95, data)
+    layers(d, 95, layers_data)
+    well(d, well_data)
     d.savePng('example.png')
 
 
+# выбор масштаба (размер секции и количество секций)
+def scaling(well_depth):
+    if well_depth <= 50:
+        section = 5
+    elif well_depth > 50 and well_depth <= 150:
+        section = 10
+    else:
+        section = 20
+    section_numbers = round((well_depth + (section/2))/section)
+    return section, section_numbers
+
 # Создание таблицы с наименованием столбцов
+
+
 def header(d):
     # d x1, y1, x2, y2
     # создание заголовка таблицы (надо немного поправить перенос строк, для таблицы
@@ -58,15 +88,8 @@ def header(d):
 
 # риусет масштабную линейку слева
 def scale(d, well_depth):
-    # выбираем масштаб
-    if well_depth <= 50:
-        section = 5
-    elif well_depth > 50 and well_depth <= 150:
-        section = 10
-    else:
-        section = 20
-    # количество делений шкалы (section/2 для округления в большую сторону)
-    section_numbers = round((well_depth + (section/2))/section)
+    # выбираем масштаб и количество делений
+    section, section_numbers = scaling(well_depth)
     # колчичество мм на секцию (вся шкала растягивается на 250 мм)
     mm_section = 250 / section_numbers
     y_start = (297 - 15 - 25 - mm_section)
@@ -154,14 +177,7 @@ def rectangle(d, x, y, x1, y1, text, direction):
 def layers(d, well_depth, dt):
     # выбираем масштаб
     data = dt['layers']
-    if well_depth <= 50:
-        section = 5
-    elif well_depth > 50 and well_depth <= 150:
-        section = 10
-    else:
-        section = 20
-    # у нас на 250 мм на листе а4 растянута полная глубина скважин с округлением
-    section_numbers = round((well_depth + (section/2))/section)
+    section, section_numbers = scaling(well_depth)
     # смещение по x: (12, 12, 12, 30, 30, 15, 15, 12, 22.5, 22.5)
     # деля 250 на округленную глубину скважины - получаем сколько в мм метров
     scale_m = (250/(section_numbers * section))
@@ -203,10 +219,47 @@ def layers(d, well_depth, dt):
         # завершение цикла отрисовки геологии
 
 
-well_data = {}
+"""well_dto = {'well_data': {
+    'columns': {
+        1: {'id': 1, 'D': 377, 'from': 0.0, 'till': 34.0, 'type': 'обсадная'},
+        2: {'id': 2, 'D': 273, 'from': 0.0, 'till': 74.0, 'type': 'обсадная'},
+        3: {'id': 3, 'D': 273, 'from': 70.0, 'till': 95.0, 'type': 'фильтровая', 'filter': {
+            1: {'id': 1, 'from': 75.0, 'till': 79.0},
+            2: {'id': 2, 'from': 85.0, 'till': 90.0},
+        }}
+    },
+    'pump_type': 'ЭЦВ-6-10-110',
+    'pump_depth': 55.0,
+    'static_lvl': 32.0,
+    'dynamic_lvl': 35.0,
+    'well_depth': 95.0
+}}"""
 
 
-#def well(d, well_depth):
+def well(d, well_dt):
+    data = well_dt['well_data']
+    # масштаб
+    section, section_numbers = scaling(data['well_depth'])
+    scale_m = (250/(section_numbers * section))
+    # стартовый диаметр внутренней колонны
+    d_start = ((30/2)/(len(data['columns'])) + 2*len(data['columns']))
+    i = 1
+    while i in data['columns']:
+        column = data['columns'][i]
+        y_start = 257 - column['from']*scale_m
+        x_start = 76 + (30-d_start)/2
+        rectangle(d, x_start, y_start, d_start,
+                  (column['from'] - column['till'])*scale_m, 'white', 'f')
+        # добавление интервалов фильтра
+        if column['type'] == 'фильтровая':
+            filter = column['filter']
+            i_f = 1
+            while i_f in filter:
+                rectangle(d, x_start, 257 - filter[i_f]['from']*scale_m, d_start,
+                          (filter[i_f]['from'] - filter[i_f]['till'])*scale_m, 'red', 'f')
+                i_f += 1
+        d_start -= 2
+        i += 1
 
 
 if __name__ == "__main__":
--- drawing/test.py
+++ drawing/test.py
@@ -1,2 +1,16 @@
-print(len('Глина серая, желтая'))
-print(30/2.1)
+well_dt = {'well_data': {
+    'columns': {
+        1: {'id': 1, 'D': 377, 'from': 0.0, 'till': 34.0, 'type': 'обсадная'},
+        2: {'id': 2, 'D': 273, 'from': 0.0, 'till': 74.0, 'type': 'обсадная'},
+        3: {'id': 3, 'D': 273, 'from': 70.0, 'till': 95.0, 'type': 'фильтровая', 'filter': {
+            1: {'id': 1, 'from': 75.0, 'till': 79.0},
+            1: {'id': 2, 'from': 85.0, 'till': 90.0},
+        }}
+    },
+    'pump_type': 'ЭЦВ-6-10-110',
+    'pump_depth': 55.0,
+    'static_lvl': 32.0,
+    'dynamic_lvl': 35.0,
+    'well_depth': 95.0
+}}
+print(well_dt['well_data']['columns'][1])
