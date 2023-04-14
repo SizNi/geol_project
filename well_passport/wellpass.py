d55f9b4212003019ed0ed3217350f7c0c69e9f8b
SizNi <wwwwwwwww@list.ru>
2023-04-14 15:35:33 +0300

    some fixes

--- well_passport/wellpass.py
+++ well_passport/wellpass.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 from docxtpl import DocxTemplate
 import json
-#from drawing.index_colours import convertation
+
+# from drawing.index_colours import convertation
 
 
 def filling_pass():
@@ -10,14 +11,15 @@ def filling_pass():
     doc = DocxTemplate("well_passport/fixtures/template_1.docx")
     with open("well_passport/fixtures/data.json") as json_file:
         data = json.load(json_file)
-        context.update(data['main_data'])
-    context['well_location'] = context['region'] + ', ' + context['district'] + ' г.о., ' + context['location']
-    context['year_now'] = datetime.now().year
-    context['layers'] = list(data['layers'].values())
+        context.update(data["main_data"])
+    context["well_location"] = (
+        context["region"] + ", " + context["district"] + " г.о., " + context["location"]
+    )
+    context["year_now"] = datetime.now().year
+    context["layers"] = list(data["layers"].values())
     print(context)
     doc.render(context)
     doc.save("well_passport/results/generated_doc.docx")
-
 
 
 filling_pass()
