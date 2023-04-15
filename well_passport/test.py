8bc1f3aa50845814ad5d2f8fa3be761c2672bc3a
SizNi <wwwwwwwww@list.ru>
2023-04-15 03:26:21 +0300

    add text for geology table

--- well_passport/test.py
+++ well_passport/test.py
@@ -1,27 +1,4 @@
-# from datetime import datetime
-from docxtpl import DocxTemplate
-import json
-
-
-def filling_pass():
-    doc = DocxTemplate("well_passport/fixtures/test_template.docx")
-    with open("well_passport/fixtures/test_data.json") as json_file:
-        data = json.load(json_file)
-        context = {
-            "col_labels": ["fruit", "vegetable", "stone", "thing"],
-            "tbl_contents": [
-                {"label": "yellow", "cols": ["banana", "capsicum", "pyrite", "taxi"]},
-                {
-                    "label": "red",
-                    "cols": ["apple", "tomato", "cinnabar", "doubledecker"],
-                },
-                {"label": "green", "cols": ["guava", "cucumber", "aventurine", "card"]},
-            ],
-        }
-
-    doc.render(context)
-    context = data
-    doc.save("well_passport/results/generated_test.docx")
-
-
-filling_pass()
+a = ["banana", "capsicum", "pyrite", "taxi"]
+print(map(str, a))
+res = ', '.join(map(str, a))
+print(res)
--- well_passport/wellpass.py
+++ well_passport/wellpass.py
@@ -12,13 +12,18 @@ def filling_pass():
     with open("well_passport/fixtures/data.json") as json_file:
         data = json.load(json_file)
         context.update(data["main_data"])
+    # сложение частей в единый адрес
     context["well_location"] = (
         context["region"] + ", " + context["district"] + " г.о., " + context["location"]
     )
+    # преобразование отложений в нормальный текст для описания (сюда надо добавить прослои и вкрапления)
+    for elem in data['layers']:
+        data['layers'][elem]['sediments'] = ', '.join(map(str, data['layers'][elem]['sediments']))
+    print(data['layers'])
     context["year_now"] = datetime.now().year
     context["layers"] = list(data["layers"].values())
-    print(context)
-    doc.render(context)
+    #print(context)
+    #doc.render(context)
     doc.save("well_passport/results/generated_doc.docx")
 
 
